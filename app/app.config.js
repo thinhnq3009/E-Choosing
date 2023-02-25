f62edd7fb80019fc264707fafe0607f6e88d7572
Thịnh Nguyễn Quốc <thinhnq3009@gmail.com>
2023-02-25 23:56:34 +0700

    Rename folder "pages" to "views"

--- app/app.config.js
+++ app/app.config.js
@@ -7,38 +7,38 @@
             // $locationProvider.html5Mode(true);
             $routeProvider
                 .when("/", {
-                    templateUrl: "pages/app-home/app-home.template.html",
+                    templateUrl: "views/app-home/app-home.template.html",
                 })
                 .when("/login", {
-                    templateUrl: "pages/app-login/app-login.template.html",
+                    templateUrl: "views/app-login/app-login.template.html",
                 })
                 .when("/signup", {
-                    templateUrl: "pages/app-signup/app-signup.template.html",
+                    templateUrl: "views/app-signup/app-signup.template.html",
                 })
                 .when("/user-info", {
-                    templateUrl: "pages/app-info/app-info.template.html",
+                    templateUrl: "views/app-info/app-info.template.html",
                     controller: "infoController",
                 })
                 .when("/doing/:code", {
-                    templateUrl: "pages/app-doing/app-doing.template.html",
+                    templateUrl: "views/app-doing/app-doing.template.html",
                     // controller: "doingController",
                 })
                 .when("/summary", {
-                    templateUrl: "pages/app-summary/app-summary.template.html",
+                    templateUrl: "views/app-summary/app-summary.template.html",
                     controller: "summaryController",
                 })
                 .when("/contract", {
-                    templateUrl: "pages/app-contract/app-contract.template.html",
+                    templateUrl: "views/app-contract/app-contract.template.html",
                 })
                 .when("/quizzes", {
-                    templateUrl: "pages/app-quizzes/app-quizzes.template.html",
+                    templateUrl: "views/app-quizzes/app-quizzes.template.html",
                     controller: "quizzesController",
                 })
                 .when("/404error", {
-                    templateUrl: "pages/app-error/404-page.html",
+                    templateUrl: "views/app-error/404-page.html",
                 })
                 .when("/test", {
-                    templateUrl: "pages/app-test/app-test.html",
+                    templateUrl: "views/app-test/app-test.html",
                     controller: "testController",
                 })
         },
